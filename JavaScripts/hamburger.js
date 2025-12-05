a4c207fbe649d62f5c1bf3b721ad93806ce743d8
Usami-Lab <usamilab.38720822@gmail.com>
2025-12-05 10:24:32 +0900

    e

---
+++ JavaScripts/hamburger.js
@@ -0,0 +1,10 @@
+console.log("hamburger.js loaded")
+$(function() {
+  $('.hamburger').click(function() {
+    // メニューの開閉状態を切り替える
+    $('.menu').toggleClass('open');
+
+    // ハンバーガーボタンのアクティブクラスを切り替えて三本線をバツにする
+    $(this).toggleClass('active');
+  });
+});
